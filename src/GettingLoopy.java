1a691f45df63e350e56124d91333632795fb72f0
tguden/Booleans <gudentc@mail.uc.edu>
2023-06-25 12:50:19 -0400

    Initial commit

---
+++ src/GettingLoopy.java
@@ -0,0 +1,11 @@
+public class GettingLoopy {
+    public static void main(String[] args)
+    {
+        //Declare Variables
+        int count;
+        System.out.println("\nCounting Loop");
+        for (count = 0; count <= 30; count++) {
+            System.out.print(count + "\t");
+        }
+    }
+}
